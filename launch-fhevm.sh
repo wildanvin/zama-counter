233dd1dd433bbe2451fa37ba7ad25ccd4dcdfd1f
Joseph-André Turk <josephandre.turk@zama.ai>
2024-09-25 16:30:14 +0200

    fix: compatibility with modified .env

--- launch-fhevm.sh
+++ launch-fhevm.sh
@@ -5,6 +5,11 @@
 # 2. All test addresses are funded (e.g. via the fund_test_addresses.sh script).
 npx hardhat clean
 mkdir -p fhevmTemp
+npx hardhat task:computeACLAddress
+npx hardhat task:computeTFHEExecutorAddress
+npx hardhat task:computeKMSVerifierAddress
+npx hardhat task:computePredeployAddress --private-key "$PRIVATE_KEY_GATEWAY_DEPLOYER"
+
 cp -L -r node_modules/fhevm fhevmTemp/
 npx hardhat compile:specific --contract fhevmTemp/fhevm/lib
 npx hardhat compile:specific --contract fhevmTemp/fhevm/gateway
@@ -12,13 +17,9 @@ mkdir -p abi
 cp artifacts/fhevmTemp/fhevm/lib/TFHEExecutor.sol/TFHEExecutor.json abi/TFHEExecutor.json
 
 PRIVATE_KEY_GATEWAY_DEPLOYER=$(grep PRIVATE_KEY_GATEWAY_DEPLOYER .env | cut -d '"' -f 2)
-npx hardhat task:computePredeployAddress --private-key "$PRIVATE_KEY_GATEWAY_DEPLOYER"
 
 npx hardhat compile:specific --contract contracts
 
-npx hardhat task:computeACLAddress
-npx hardhat task:computeTFHEExecutorAddress
-npx hardhat task:computeKMSVerifierAddress
 npx hardhat task:deployACL
 npx hardhat task:deployTFHEExecutor
 npx hardhat task:deployKMSVerifier
